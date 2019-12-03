52090b25756e222199451ada7d336ee1e909eec7
Adam DeLessio <adamdelessio@gmail.com>
2019-12-03 16:58:04 -0500

    Made basic design

---
+++ script.js
@@ -0,0 +1,72 @@
+
+let blocks = document.querySelectorAll('.main-block')
+
+let blockArray = []
+
+for (let i = 0; i < blocks.length; i++) {
+	blockArray.push(blocks[i])
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/// Draggable stuff
+
+// blockArray[0].addEventListener('mouseover', makeDraggable)
+// function makeDraggable() {
+// 	document.querySelector('.block').setAttribute('draggable', 'true')
+// 	stopDrag()
+// }
+
+
+
+
+
+
+
