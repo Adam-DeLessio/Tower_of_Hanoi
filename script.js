7d9695ebae7b1302f70b8164926704c38348a668
Adam DeLessio <adamdelessio@gmail.com>
2019-12-04 18:17:09 -0500

    Working on compare logic

--- script.js
+++ script.js
@@ -1,47 +1,34 @@
 
-/// Array of the 3 boxes
-let boxes = document.querySelectorAll('.box')
+/// Makes array of the 3 boxes
+let box = document.querySelectorAll('.box')
 
-/// No blocks are selected
+/// Empty variable that represents any block
 let block = null
+let blockValue = null
 
 /// Makes each box clickable
-for (let i = 0; i < boxes.length; i++) {
-	boxes[i].addEventListener('click', moveBlock)
+for (let i = 0; i < box.length; i++) {
+	box[i].addEventListener('click', moveBlock)
 }
 
 /// Selects the top block in the selected box
 function moveBlock(event) {
-	if (block === null && (event.target === boxes[0].firstElementChild || event.target === boxes[1].firstElementChild || event.target === boxes[2].firstElementChild)) {
+	if (block === null && (event.target === box[0].firstElementChild || event.target === box[1].firstElementChild || event.target === box[2].firstElementChild)) {
 		block = event.target
 		block.style.backgroundColor = 'gray'
-	} else if (block !== null && (event.target === boxes[0] || event.target === boxes[1] || event.target === boxes[2])) {
-		
+		blockValue = block.getAttribute('data-value')
+	} else if (block !== null && (event.target === box[0] || event.target === box[1] || event.target === box[2])) {
+
+		let newBox = event.target
+
+		checkBox()
+
 		event.target.insertBefore(block, event.target.firstElementChild)
 		block.style.backgroundColor = 'black'
 		block = null
+		blockValue = null
 	}
 }
-
-
- 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
