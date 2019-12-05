2d53ca9af9130da9098d849cbe93f0976a18ab03
Adam DeLessio <adamdelessio@gmail.com>
2019-12-05 10:36:49 -0500

    Stacking logic works

--- script.js
+++ script.js
@@ -2,9 +2,11 @@
 /// Makes array of the 3 boxes
 let box = document.querySelectorAll('.box')
 
-/// Empty variable that represents any block
 let block = null
+let newBox = null
+let newBoxvalue = null
 let blockValue = null
+let topValue = null
 
 /// Makes each box clickable
 for (let i = 0; i < box.length; i++) {
@@ -16,22 +18,43 @@ function moveBlock(event) {
 	if (block === null && (event.target === box[0].firstElementChild || event.target === box[1].firstElementChild || event.target === box[2].firstElementChild)) {
 		block = event.target
 		block.style.backgroundColor = 'gray'
-		blockValue = block.getAttribute('data-value')
+		blockValue = event.target.getAttribute('data-value')
 	} else if (block !== null && (event.target === box[0] || event.target === box[1] || event.target === box[2])) {
+		newBox = event.target
+		newBoxvalue = event.target.firstElementChild
 
-		let newBox = event.target
-
-		checkBox()
-
-		event.target.insertBefore(block, event.target.firstElementChild)
-		block.style.backgroundColor = 'black'
-		block = null
-		blockValue = null
+		if (event.target.children.length > 0) {
+			topValue = event.target.firstElementChild.getAttribute('data-value')
+		}
+		compare()
 	}
 }
 
-
-
+function compare() {
+	if (newBoxvalue === null) {
+		newBox.appendChild(block)
+		block.style.backgroundColor = 'black'
+		block = null
+		newBox = null
+		newBoxvalue = null
+	} else if (newBoxvalue !== null && topValue > blockValue) {
+		newBox.insertBefore(block, newBox.firstElementChild)
+		block.style.backgroundColor = 'black'
+		block = null
+		newBox = null
+		newBoxvalue = null
+	} else if (newBoxvalue !== null && topValue === blockValue) {
+		block.style.backgroundColor = 'black'
+		block = null
+		newBox = null
+		newBoxvalue = null
+	} else if (newBoxvalue !== null && topValue < blockValue) {
+		block.style.backgroundColor = 'black'
+		block = null
+		newBox = null
+		newBoxvalue = null
+	}
+}
 
 
 
